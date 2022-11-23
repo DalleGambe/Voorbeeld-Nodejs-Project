7e3e91bd3b41fd239c11221881f734a2b119740e
DalleGambe <dallegambe@gmail.com>
2022-11-23 14:36:56 +0100

    added artifact to test

--- test/test.js
+++ test/test.js
@@ -7,7 +7,16 @@ describe('get campus', () => {
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8')  
         .expect((res) => {
-            console.log(res.body[0])
-        })
+            fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
+                if (err) {
+                    console.log("An error occured while writing JSON Object to file.");
+                    return console.log(err);
+                }
+
+            console.log("JSON file has been saved.");            
+        });
+
+        console.log(res.body[0])
+    })
     });
 });
