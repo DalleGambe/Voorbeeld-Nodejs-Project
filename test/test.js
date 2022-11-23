ce3650404644a158852554baf86325430142737f
DalleGambe <dallegambe@gmail.com>
2022-11-23 14:53:20 +0100

    changed fs to filesystem

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('get campus', () => {
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8')  
         .expect((res) => {
-            fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
+            FileSystem.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
                 if (err) {
                     console.log("An error occured while writing JSON Object to file.");
                     return console.log(err);
